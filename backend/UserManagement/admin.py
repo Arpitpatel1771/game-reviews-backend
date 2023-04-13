fc032ed631e0f4fcf94b1011f8834016a4d2383d
Arpit Patel <arpitpatel1771@gmail.com>
2023-04-13 11:51:14 +0530

    admindoc app experiments

--- backend/UserManagement/admin.py
+++ backend/UserManagement/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from UserManagement.models import User
+from UserManagement.models import User, TestClass
 
 # Register your models here.
 admin.site.register(User, UserAdmin)
+
+@admin.register(TestClass)
+class TestClassAdmin(admin.ModelAdmin):
+    list_display = ["id", "name"]
--- backend/UserManagement/models.py
+++ backend/UserManagement/models.py
@@ -4,4 +4,8 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     pass
 
-
+class TestClass(models.Model):
+    name = models.CharField(max_length=250, blank=False, null=False, choices=(("A", "A"),("B", "B")), 
+                            help_text="Name of an entity in this model. E.g.: Arpit")
+    user = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)
+    friends = models.ManyToManyField(User, related_name="test", blank=True)
